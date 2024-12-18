0e0e85cc0a3555fc862a0a9efad1ed0bed69ea3e
daniel-dieguez <danguez2001@gmail.com^C>
2024-12-18 17:41:54 -0600

    Realice codigo para contenerizar la aplicacion

--- Gestiones/obj/Debug/net8.0/Gestiones.AssemblyInfo.cs
+++ Gestiones/obj/Debug/net8.0/Gestiones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestiones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14672e139ee03c757769ca8b7e1e57122c535d18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestiones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestiones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
